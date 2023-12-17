968888f87c7ec303bd767784ca3441104ad6ec0b
Samuel Rembisz <sjrembisz07@gmail.com>
2023-12-17 11:52:58 -0500

    chore: allow all date/time types to be sorted

--- src/lib.rs
+++ src/lib.rs
@@ -439,7 +439,8 @@ pub fn create_filter(input: TokenStream) -> TokenStream {
                         .into_iter()
                         .any(|token| match token {
                             proc_macro2::TokenTree::Ident(ident) => {
-                                ident.to_string().as_str() == "NaiveDateTime" || ident.to_string().as_str() == "NaiveDate"
+                                // NaiveDate/Time, Date/Time<_>
+                                ident.to_string().contains("Date")
                             }
                             _ => false,
                         }) {
